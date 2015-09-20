a7017e6e60654449b43bd8ec99e48400306acd77
AkiPraveen <akilesh.praveen@gmail.com>
2015-09-20 15:29:59 -0700

    updates
    initial updating

---
+++ js/script.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+	Parse.initialize("MFgxDOAeaC1MGx5YNgRyDtfBkJLOfNowfIFAKEwf", "4cmF2z7jyMoUsKmSK7cxpxeNptMbqPQ2bqzKFzLb");
+
+	init();
+
+	
+});
+
+function init(){
+	var Picture = Parse.Object.extend("Picture");
+	var picture = new Parse.Query(Picture);
+	picture.equalTo("use", true);
+	picture.find({
+	  success: function(results) {
+	    //alert("Successfully retrieved " + results.length + " scores.");
+	    // Do something with the returned Parse.Object values
+	    for (var i = 0; i < results.length; i++) {
+
+	    	var obj = results[i];
+	    	var image = obj.get("file");
+			var url = image.url();
+			//alert(url);
+
+	      $("#grid").html("<li><img src=\"" + url + "\"></a></li>" + $("#grid").html());
+
+	      new AnimOnScroll( document.getElementById( 'grid' ), {
+				minDuration : 0.4,
+				maxDuration : 0.7,
+				viewportFactor : 0.2
+			} );
+	    }
+	
+	  },
+	  error: function(error) {
+	    //alert("Error: " + error.code + " " + error.message);
+	  }
+	});
+}
+
+function submit(){
+	//alert("submit clicked");
+	var fileUploadControl = $("#pictureHolder")[0];
+	if (fileUploadControl.files.length > 0) {
+		var file = fileUploadControl.files[0];
+		var name = "genericFileName";
+
+		var parseFile = new Parse.File(name, file);
+		parseFile.save().then(function() {
+				// The file has been saved to Parse.
+				//alert("Saved");
+			}, function(error) {
+				// The file either could not be read, or could not be saved to Parse.
+
+		});
+
+		var Picture = Parse.Object.extend("Picture");
+		var picture = new Picture();
+
+		picture.set("file", parseFile);
+		picture.set("use", true);
+
+		picture.save(null, {
+			success: function(gameScore) {
+				// Execute any logic that should take place after the object is saved.
+				//alert('New object created with objectId: ' + gameScore.id);
+				location.reload();
+				},
+			error: function(gameScore, error) {
+				// Execute any logic that should take place if the save fails.
+				// error is a Parse.Error with an error code and message.
+				//alert('Failed to create new object, with error code: ' + error.message);
+			}
+		});
+
+		
+	}
+
+	
+
+
+}
